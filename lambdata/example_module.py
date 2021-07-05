78944e40911356287437bb9134808d003dc18183
ivan-mihailov <ivan.i.mihailov@gmail.com>
2021-07-05 13:47:24 -0400

    Adding lambdata package

---
+++ lambdata/example_module.py
@@ -0,0 +1,12 @@
+COLORS = ['Blue', 'Pink', 'Yellow']
+
+def increment(x):
+    """Adds 1 to x"""
+    return x + 1
+
+# def strip_letters(x):
+#     '''Strips 3 letters from beginning of x'''
+#     lstrip
+
+def multiply(x):
+    return x*2
